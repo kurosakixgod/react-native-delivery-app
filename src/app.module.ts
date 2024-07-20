eda8610abd089a6d533cbba0b7a75b381ded5383
larr1 <kurochkin.vasiliy90@gmail.com>
2024-07-20 23:33:35 +0300

    added and done categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { ConfigModule } from "@nestjs/config";
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-	imports: [AuthModule, ConfigModule.forRoot()],
+	imports: [AuthModule, ConfigModule.forRoot(), CategoryModule, ProductModule, UserModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
